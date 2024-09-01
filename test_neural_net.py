832030dceef3c3663a0313d8e34a524c90da1584
Joshua Sonnen <153052192+jsonnen2@users.noreply.github.com>
2024-09-01 16:41:42 -0700

    Create test_neural_net.py

---
+++ test_neural_net.py
@@ -0,0 +1,7 @@
+"""
+Tests all models from the \\models dump folder. 
+I need an info file which can store
+-- policy architecture
+-- all dataclass args
+-- training info -> num_iterations, CPU usage, etc. (wandb upload)
+"""
