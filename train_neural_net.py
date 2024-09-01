79a0c17155370b3cad7576c7de7d5ea0fc9c007d
Joshua Sonnen <153052192+jsonnen2@users.noreply.github.com>
2024-09-01 16:38:09 -0700

    Update train_neural_net.py

--- train_neural_net.py
+++ train_neural_net.py
@@ -2,6 +2,7 @@
 PPO script.
 Imports: numpy, torch, tyro (dataclass)
 class Args-- dataclass for args
+  -- I want to expose the dataclass to the command line so that the user can specify ANY arg from a command line call
 class CNN-- pytorch conv net. with layer_init. get_value(). forward().
 class Env-- my custom env wrapper. preprocess. run multiple envs in parallel. framestacking.
 import ppo from stable baselines 3
